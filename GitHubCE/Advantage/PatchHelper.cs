abdef14164753a81e99b62923467819287c3bb81
RobRobertsCE <rroberts@centeredgesoftware.com>
2016-04-29 11:53:38 -0400

    wip1

---
+++ GitHubCE/Advantage/PatchHelper.cs
@@ -0,0 +1,246 @@
+﻿using DbVersionLibrary;
+using GitHubCE.Logic;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace GitHubCE.Advantage
+{
+
+    public class PatchHelper
+    {
+        #region consts
+        // c:\users\rroberts\patchBackups\Advantage\16\16.2.21.215\04232016-1123\
+        private const string AssmeblyBackupPathTemplate = @"C:\Users\{0}\PatchBackups\{1}\{2}\{3}\{4}\";
+        private const string RepoPathTemplate = @"C:\Users\{0}\Source\Repos\{1}";
+        private const string BinPathTemplate = @"C:\Users\{0}\Source\Repos\{1}\bin";
+        private const string PatchFolderTemplate = @"\\ceserver\Development\Test Program Setups\{0}\{1}\Version {2}\Patch";
+        private const string VersionFileTemplate = @"C:\Users\{0}\Source\Repos\{1}\src\version.txt";
+        #endregion
+
+        #region fields
+        IList<FileInfo> _sourceAssemblies = new List<FileInfo>();
+        IList<FileInfo> _targetAssemblies = new List<FileInfo>();
+        DateTime _startupTimestamp;
+        #endregion
+
+        #region properties
+        public PullRequestAssembyHelper AssemblyHelper { get; private set; }
+        public Version PatchTargetVersion { get; set; }
+        public DateTime BuildTimestampLimit { get; set; }
+        #endregion
+
+        #region read only properties
+        public string RepoName
+        {
+            get
+            {
+                return AssemblyHelper.RepoName;
+            }
+        }
+        public string WinUserName
+        {
+            get
+            {
+                return AssemblyHelper.WinUserName;
+            }
+        }
+        public Version RepoVersion
+        {
+            get
+            {
+                var versionFileContent = File.ReadAllText(VersionFile);
+                return new Version(versionFileContent);
+            }
+        }
+        public string RepoFolder
+        {
+            get
+            {
+                return String.Format(RepoPathTemplate, WinUserName, RepoName);
+            }
+        }
+        public string BinFolder
+        {
+            get
+            {
+                return String.Format(BinPathTemplate, WinUserName, RepoName);
+            }
+        }
+        public string PatchFolder
+        {
+            get
+            {
+                return String.Format(PatchFolderTemplate, RepoName, RepoVersion.Major.ToString(), RepoVersion.ToString());
+            }
+        }
+        public string VersionFile
+        {
+            get
+            {
+                return String.Format(VersionFileTemplate, WinUserName, RepoName);
+            }
+        }
+        public string AssemblyBackupFolder
+        {
+            get
+            {
+                return String.Format(AssmeblyBackupPathTemplate, WinUserName, RepoName, PatchTargetVersion.Major, PatchTargetVersion, _startupTimestamp.ToString("yyyyMMdd-hhmm"));
+            }
+        }
+        public IList<FileInfo> SourceAssemblies
+        {
+            get
+            {
+                if (null== _sourceAssemblies)
+                    _sourceAssemblies = GetSourceAssmeblies();
+                return _sourceAssemblies;
+            }
+        }
+        #endregion
+
+        #region ctor       
+        public PatchHelper(PullRequestAssembyHelper assemblyHelper, Version patchTargetVersion)
+        {
+            this.AssemblyHelper = assemblyHelper;
+            this.PatchTargetVersion = patchTargetVersion;
+            this.BuildTimestampLimit = AssemblyHelper.PullRequest.Updated.DateTime.ToLocalTime();
+            _startupTimestamp = DateTime.Now;
+        }
+        #endregion
+
+        #region private
+        public TaskResult ValidatePatchProcess()
+        {
+            var result = new TaskResult() { IsValid = true };
+            // pull request merged?
+            if (!AssemblyHelper.PullRequest.Merged)
+            {
+                result.AddError(String.Format("Pull Request {0} has not been merged yet.", AssemblyHelper.PullRequest.Id));
+                // no further validation may be done. Bail...
+                return result;
+            }
+
+            try
+            {
+                // repo at the correct version?
+                if (PatchTargetVersion.Build == -1 && PatchTargetVersion.Revision == -1)
+                {
+                    if (RepoVersion.Major != PatchTargetVersion.Major || RepoVersion.Minor != PatchTargetVersion.Minor)
+                    {
+                        result.AddError(String.Format("Repo version, {0}, does match patch target version, {1}.", RepoVersion.ToString(), PatchTargetVersion.ToString()));
+                        // no further validation may be done. Bail...
+                        return result;
+                    }
+                    else
+                    {
+                        PatchTargetVersion = RepoVersion;
+                    }
+                }
+
+                _sourceAssemblies = GetSourceAssmeblies();
+                // assembly exists?                
+                foreach (var assemblyFileName in _sourceAssemblies)
+                {
+                    if (!File.Exists(assemblyFileName.FullName))
+                    {
+                        result.AddError(String.Format("Built assembly {0} is not found.", assemblyFileName.FullName));
+                    }
+                    else
+                    {
+                        // versions correct?
+                        var versInfo = FileVersionInfo.GetVersionInfo(assemblyFileName.FullName);
+                        String fileVersion = versInfo.FileVersion;
+                        String productVersion = versInfo.ProductVersion;
+
+                        //example for own display version string, built of the four version parts:
+                        String myVers = String.Format("V{0}.{1}.{2} build {3}", versInfo.FileMajorPart, versInfo.FileMinorPart,
+                                                                                 versInfo.FileBuildPart, versInfo.FilePrivatePart);
+
+                        Console.WriteLine("{0}: File Version:{1}; Product Version:{2}; myVers:{3}; IsDebug:{4}", assemblyFileName.FullName, fileVersion, productVersion, myVers, versInfo.IsDebug);
+                        if (PatchTargetVersion.ToString() != fileVersion)
+                        {
+                            result.AddError(String.Format("Built assembly {0} is at version {1}, patching to version {2}.", assemblyFileName, fileVersion, PatchTargetVersion.ToString()));
+                        }
+
+                        // built since request made?
+                        var binAssemblyFileInfo = new FileInfo(assemblyFileName.FullName);
+                        DateTime binAssemblyLastModified = binAssemblyFileInfo.LastWriteTime;
+                        if (BuildTimestampLimit > binAssemblyLastModified)
+                        {
+                            result.AddError(String.Format("Built assembly {0} is out of date. Last build was {1}, build cutoff timestamp is {2}.", assemblyFileName.FullName, binAssemblyLastModified, BuildTimestampLimit));
+                        }
+
+                        // debug?
+                        if (versInfo.IsDebug)
+                        {
+                            result.AddError(String.Format("Built assembly {0} is a DEBUG assembly.", assemblyFileName.FullName));
+                        }
+
+                        var targetAssembly = Path.Combine(PatchFolder, assemblyFileName.Name);
+                        if (File.Exists(targetAssembly))
+                        {
+                            result.AddDebug(String.Format("Assembly {0} exists in patch folder.", targetAssembly));
+                            // target assembly not newer than source assembly?
+                            var patchFolderAssemblyFileInfo = new FileInfo(targetAssembly);
+
+                            if (binAssemblyLastModified < patchFolderAssemblyFileInfo.LastWriteTime)
+                            {
+                                result.AddError(String.Format("Built assembly {0} is out of date. Assembly currently in Patch folder has more recent build timestamp, {1}.", assemblyFileName.Name, binAssemblyLastModified, patchFolderAssemblyFileInfo.LastWriteTime));
+                            }
+                            // target assembly build timestamp same as source? (same build time and version)
+                            if (binAssemblyLastModified == patchFolderAssemblyFileInfo.LastWriteTime)
+                            {
+                                result.AddError(String.Format("Assembly {0} has the same build timestamp in Patch and Bin folders.", assemblyFileName.Name));
+                            }
+                        }
+
+                        // confirm commit(s) in current branch.
+
+
+                        if (!Directory.Exists(AssemblyBackupFolder))
+                        {
+                            var backupInfo = Directory.CreateDirectory(AssemblyBackupFolder);
+                            if (null==backupInfo)
+                            {
+                                result.AddError(String.Format("Error creating backup directory {0}.", AssemblyBackupFolder));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result.AddError(ex.ToString());
+            }
+            return result;
+        }
+
+        IList<FileInfo> GetSourceAssmeblies()
+        {
+            var sourceFiles = new List<FileInfo>();
+
+            foreach (var assemblyFileName in AssemblyHelper.AssemblyFiles)
+            {
+                if (!assemblyFileName.Contains("AdvUpgrade"))
+                {
+                    var builtAssemblyFilePath = Path.Combine(BinFolder, assemblyFileName);
+                    sourceFiles.Add(new FileInfo(builtAssemblyFilePath));
+                }
+            }
+
+            return sourceFiles;
+        }
+
+        IList<FileInfo> GetExistingTargetAssmeblies()
+        {
+            var targetFiles = new List<FileInfo>();
+
+
+            return targetFiles;
+        }
+        #endregion
+
+    }
+}
---
+++ GitHubCE/Forms/PatchFileMoverDialog.cs
@@ -0,0 +1,259 @@
+﻿using GitHubCE.Advantage;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GitHubCE.Forms
+{
+    public partial class PatchFileMoverDialog : Form
+    {
+        PatchHelper PatchHelper { get; set; }
+        bool _passedValidation = false;
+
+        #region ctor / load
+        public PatchFileMoverDialog()
+        {
+            InitializeComponent();
+        }
+        public PatchFileMoverDialog(PatchHelper patchHelper)
+        {
+            InitializeComponent();
+
+            this.PatchHelper = patchHelper;
+        }
+        private void PatchFileMoverDialog_Load(object sender, EventArgs e)
+        {
+            ValidatePatch();
+            RefreshLists();
+        }
+        #endregion
+
+        #region refresh lists
+        void RefreshLists()
+        {
+            PopulateSource();
+            PopulateTarget();
+        }
+        void PopulateSource()
+        {
+            lvSource.Groups.Clear();
+            var groupName = String.Format("{0} {1}", PatchHelper.RepoName, PatchHelper.RepoVersion);
+            lvSource.Groups.Add(new ListViewGroup(groupName, HorizontalAlignment.Left));
+            foreach (var sourceFile in PatchHelper.SourceAssemblies)
+            {
+                var versionInfo = FileVersionInfo.GetVersionInfo(sourceFile.FullName);
+                var sourceFileInfo = new FileInfo(sourceFile.FullName);
+                var lvi = new ListViewItem(new[] { sourceFile.Name, versionInfo.FileVersion, sourceFileInfo.LastWriteTime.ToString() });
+                lvi.Group = lvSource.Groups[0];
+                lvi.ImageIndex = 7;
+                lvSource.Items.Add(lvi);
+            }
+        }
+        void PopulateTarget()
+        {
+            var filesBeingPatched = PatchHelper.SourceAssemblies.Select(s => s.Name).ToList();
+
+            lvDestination.Groups.Clear();
+
+            lvDestination.Groups.Add(new ListViewGroup(Path.GetDirectoryName(PatchHelper.PatchFolder), HorizontalAlignment.Left));
+            foreach (var patchFolderFile in Directory.GetFiles(PatchHelper.PatchFolder))
+            {
+                var versionInfo = FileVersionInfo.GetVersionInfo(patchFolderFile);
+                var sourceFileInfo = new FileInfo(patchFolderFile);
+                var lvi = new ListViewItem(new[] { Path.GetFileName(patchFolderFile), versionInfo.FileVersion, sourceFileInfo.LastWriteTime.ToString() });
+                lvi.Group = lvDestination.Groups[0];
+                if (patchFolderFile.EndsWith(".dll"))
+                {
+                    lvi.ImageIndex = 3;
+                }
+                else if (patchFolderFile.EndsWith(".exe"))
+                {
+                    lvi.ImageIndex = 2;
+                }
+                else if (patchFolderFile.EndsWith(".txt"))
+                {
+                    lvi.ImageIndex = 8;
+                }
+                else
+                {
+                    lvi.ImageIndex = 8;
+                }
+
+                // highlight files being patched.
+                if (filesBeingPatched.Contains(Path.GetFileName(patchFolderFile)))
+                {
+                    lvi.BackColor = Color.Silver;
+                    lvi.UseItemStyleForSubItems = true;
+                }
+
+                lvDestination.Items.Add(lvi);
+            }
+        }
+        #endregion
+
+        #region validate patch
+        void ValidatePatch()
+        {
+            ClearError();
+            tsbMoveFiles.Enabled = false;
+            _passedValidation = false;
+
+            var validationResults = PatchHelper.ValidatePatchProcess();
+
+            StringBuilder sb = new StringBuilder();
+            if (validationResults.IsValid)
+            {
+                sb.AppendLine("Validation Passed");
+                tsbMoveFiles.Enabled = true;
+                _passedValidation = true;
+            }
+            else
+            {
+                tsbMoveFiles.Enabled = false;
+                _passedValidation = false;
+
+                DisplayError("***** Validation Failed *****");
+                foreach (var message in validationResults.Errors)
+                    sb.AppendLine(message);
+            }
+
+            if (validationResults.HasDebugMessages)
+            {
+                sb.AppendLine("***** DEBUG *****");
+                foreach (var message in validationResults.DebugMessages)
+                {
+                    sb.AppendLine(message);
+                }
+            }
+            var validationReport = sb.ToString();
+            Console.WriteLine(validationReport);
+            DisplayMessage(validationReport);
+        }
+        #endregion
+
+        #region update patch
+        void UpdatePatchFiles()
+        {
+            try
+            {
+                if (!_passedValidation)
+                {
+                    DisplayMessage("Validate Before Running");
+                    return;
+                }
+
+                // backup files.
+                foreach (var patchFolderFile in Directory.GetFiles(PatchHelper.PatchFolder))
+                {
+                    var backupFileName = Path.Combine(PatchHelper.AssemblyBackupFolder, Path.GetFileName(patchFolderFile));
+                    File.Copy(patchFolderFile, backupFileName);
+                }
+
+                // copy files
+                foreach (var sourceFile in PatchHelper.SourceAssemblies)
+                {
+                    var patchFileName = Path.Combine(PatchHelper.PatchFolder, sourceFile.Name);
+                    DisplayMessage(String.Format("***** COPYING {0} TO {1} *********",sourceFile.FullName, patchFileName));
+                    File.Copy(sourceFile.FullName, patchFileName, true);
+                }
+
+                // refresh patch folder file list.
+                PopulateTarget();
+            }
+            catch (Exception ex)
+            {
+                ExceptionHandler(ex);
+            }
+        }
+
+        void ShowBackups()
+        {
+            Process.Start(PatchHelper.AssemblyBackupFolder);
+        }
+        void ShowPatchFolder()
+        {
+            Process.Start(PatchHelper.PatchFolder);
+        }
+        void ShowBinFolder()
+        {
+            Process.Start(PatchHelper.BinFolder);
+        }
+        void OpenPatchNotes()
+        {
+            Process.Start(Path.Combine(PatchHelper.PatchFolder, "Patch Notes.txt"));
+        }
+        #endregion
+
+        #region common
+        void DisplayMessage(string message)
+        {
+            txtMessages.Text += message + "\r\n";
+            txtMessages.SelectionStart = txtMessages.TextLength;
+            txtMessages.ScrollToCaret();
+        }
+        void DisplayError(string errorMessage)
+        {
+            lblError.Text = errorMessage;
+        }
+        void ClearError()
+        {
+            lblError.Text = "";
+        }
+        void ExceptionHandler(Exception ex)
+        {
+            DisplayMessage(ex.Message);
+            Console.WriteLine(ex.ToString());
+        }
+        #endregion
+
+        #region button events
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void tsbValidate_Click(object sender, EventArgs e)
+        {
+            ValidatePatch();
+        }
+
+        private void tsbMoveFiles_Click(object sender, EventArgs e)
+        {
+            UpdatePatchFiles();
+        }
+
+        private void tsbRefresh_Click(object sender, EventArgs e)
+        {
+            RefreshLists();
+        }
+
+        private void tsbBackups_Click(object sender, EventArgs e)
+        {
+            ShowBackups();
+        }
+
+        private void tsbPatchFolder_Click(object sender, EventArgs e)
+        {
+            ShowPatchFolder();
+        }
+
+        private void tsbBinFolder_Click(object sender, EventArgs e)
+        {
+            ShowBinFolder();
+        }
+
+        private void tsbPatchNotes_Click(object sender, EventArgs e)
+        {
+            OpenPatchNotes();
+        }
+        #endregion
+    }
+}
